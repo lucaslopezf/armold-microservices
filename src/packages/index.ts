109c391fbf098dfdc5378d7c05c959420c167537
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-12-07 01:45:47 -0300

    minor fixes

--- src/packages/index.ts
+++ src/packages/index.ts
@@ -2,3 +2,4 @@ export * from './error';
 export * from './logging';
 export * from './middlewares';
 export * from './server';
+export * from './utils';
---
+++ src/packages/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './helper';
+export * from './http/httpClient';
+export * from './validator';
